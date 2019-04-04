24c58bb24041adf36bb2bdebc8dbfb5bf399bfaf
TheGreatRambler <aehart684351@gmail.com>
2019-04-04 15:44:50 -0400

    TABS > SPACES

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Library.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Library.java
@@ -5,70 +5,70 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public abstract class Library extends OpMode {
-    // Declare Hardware Devices
-    public static DcMotor frontLeft, frontRight, backLeft, backRight;
+	// Declare Hardware Devices
+	public static DcMotor frontLeft, frontRight, backLeft, backRight;
 
-    // Declare initializing method
-    public void hardwareInit() {
-        frontLeft = hardwareMap.dcMotor.get("m0");
-        frontRight = hardwareMap.dcMotor.get("m1");
-        backLeft = hardwareMap.dcMotor.get("m2");
-        backRight = hardwareMap.dcMotor.get("m3");
-    }
-    //Declare other helper methods
+	// Declare initializing method
+	public void hardwareInit() {
+		frontLeft = hardwareMap.dcMotor.get("m0");
+		frontRight = hardwareMap.dcMotor.get("m1");
+		backLeft = hardwareMap.dcMotor.get("m2");
+		backRight = hardwareMap.dcMotor.get("m3");
+	}
+	// Declare other helper methods
 
 
-    public static void drive(float lf, float rf, float lb, float rb) {
-    /*
-    Provides basic motor power to all 4 motors.
-    @param: float lf, rf, lb & rb all -1<x<1
-    @return: void
-     */
-        frontLeft.setPower(-lf);
-        frontRight.setPower(rf);
-        backLeft.setPower(-lb);
-        backRight.setPower(rb);
+	public static void drive(float lf, float rf, float lb, float rb) {
+		/*
+		Provides basic motor power to all 4 motors.
+		@param: float lf, rf, lb & rb all -1<x<1
+		@return: void
+		 */
+		frontLeft.setPower(-lf);
+		frontRight.setPower(rf);
+		backLeft.setPower(-lb);
+		backRight.setPower(rb);
 
-        //power settings for motors.
-    }
+		// power settings for motors.
+	}
 
-    public static void omni(float l, float r, float s) {
-    /*
-    Omni-driving function.
-    @param: l, linear component, r, rotational component, and s, horizontal component
-     */
-        float[] forwardMultiplier = {1f, 1f, 1f, 1f};
-        float[] rotationalMultiplier = {-1f, 1f, -1f, 1f};
-        float[] horizontalMultiplier = {-1f, 1f, 1f, -1f};
+	public static void omni(float l, float r, float s) {
+		/*
+		Omni-driving function.
+		@param: l, linear component, r, rotational component, and s, horizontal component
+		 */
+		float[] forwardMultiplier = { 1f, 1f, 1f, 1f };
+		float[] rotationalMultiplier = { -1f, 1f, -1f, 1f };
+		float[] horizontalMultiplier = { -1f, 1f, 1f, -1f };
 
-        float[] forwardComponent = new float[4];
-        float[] rotationalComponent = new float[4];
-        float[] eastwestComponent = new float[4];
+		float[] forwardComponent = new float[4];
+		float[] rotationalComponent = new float[4];
+		float[] eastwestComponent = new float[4];
 
-        for (int i = 0; i < 4; i++) {
-            forwardComponent[i] = forwardMultiplier[i] * l;
-            rotationalComponent[i] = rotationalMultiplier[i] * r;
-            eastwestComponent[i] = horizontalMultiplier[i] * s;
-        }
+		for (int i = 0; i < 4; i++) {
+			forwardComponent[i] = forwardMultiplier[i] * l;
+			rotationalComponent[i] = rotationalMultiplier[i] * r;
+			eastwestComponent[i] = horizontalMultiplier[i] * s;
+		}
 
-        float[] sums = new float[4];
-        for (int i = 0; i < 4; i++) {
-            sums[i] += forwardComponent[i] + rotationalComponent[i] + eastwestComponent[i];
-        }
+		float[] sums = new float[4];
+		for (int i = 0; i < 4; i++) {
+			sums[i] += forwardComponent[i] + rotationalComponent[i] + eastwestComponent[i];
+		}
 
-        float highest = maxValue(sums);
+		float highest = maxValue(sums);
 
-        if (Math.abs(highest) > 1) {
-            attenuationfactor = highest;
-        } else {
-            attenuationfactor = 1f;
-        }
+		if (Math.abs(highest) > 1) {
+			attenuationfactor = highest;
+		} else {
+			attenuationfactor = 1f;
+		}
 
-        for (int i = 0; i < 4; i++) {
-            sums[i] = sums[i] / attenuationfactor;
-        }
+		for (int i = 0; i < 4; i++) {
+			sums[i] /= attenuationfactor;
+		}
 
-        drive(sums[0], sums[1], sums[2], sums[3]);
+		drive(sums[0], sums[1], sums[2], sums[3]);
 
-    } /* OMNI IS WHAT WE USE, DRIVE IS FOR OMNI TO USE*/
+	} /* OMNI IS WHAT WE USE, DRIVE IS FOR OMNI TO USE*/
 }
