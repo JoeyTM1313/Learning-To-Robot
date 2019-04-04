607f9bf9876985eb10649a506bec294148ab5181
JoeyTM1313 <42985043+JoeyTM1313@users.noreply.github.com>
2019-04-04 15:31:11 -0400

    Start of Library
    Library is in Teamcode.java.org.firstinspires.ftc.teamcode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Library.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public abstract class Library extends OpMode {
+    // Declare Hardware Devices
+    public static DcMotor topRight, bottomRight, topLeft, bottomLeft;
+    // Declare initializing method
+    public void hardwareInit(){
+        topRight = HardwareMap.
+    }
+    //Declare other helper methods
+
+
+
+
+}
