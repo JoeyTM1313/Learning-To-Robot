fcc76495e8ec9d30d193d58637e4de8fbaffa85e
alko08 <alexk@thekoppels.net>
2019-04-09 19:14:48 -0400

    Added Array list of joystick vaules
    title

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp.java
@@ -1,9 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import java.util.ArrayList;
+
 @TeleOp(name = "teleOp")
 public class teleOp extends Library {
 
+    ArrayList<float[]> tripleValues = new ArrayList<>(); //array must be three long
     public void init() {
         hardwareInit();
     }
@@ -13,9 +16,15 @@ public class teleOp extends Library {
         float side = gamepad1.left_stick_x;
         float rotation = gamepad1.right_stick_x;
         omni(linear, rotation, side);
+        float[] floatArray = new float[3];
+        floatArray[0]=linear;
+        floatArray[1]=side;
+        floatArray[2]=rotation;  //array of three to save values into arrayList tripleValues
+        tripleValues.add(floatArray);
+        telemetry.addData("l,s,r respectively ",floatArray);
 
         //defining the stuff. linear = straight, rotation = turning, side = skating.
-        //Linear - rotation will compensate one side to allow the other side to overrotate
+        //Linear - rotation will compensate one side to allow the other side to overrate
 
     }
 }
